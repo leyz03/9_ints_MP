2fe8b8ccd6d7a819ce87edcb9febc2e68cf6376f
leyz03 <zzzjl512@gmail.com>
2025-03-22 12:07:42 -0700

    modify env_nums=8

--- plot_rewards.py
+++ plot_rewards.py
@@ -11,8 +11,8 @@ path_convert = get_abs_path(__file__)
 
 if __name__ == '__main__':
     log_labels = {
-        "Exp1": [path_convert(f'./log/Exp1-demo/{i}.monitor.csv')for i in range(16)],
-        "Exp2": [path_convert(f'./log/Exp2-demo/{i}.monitor.csv')for i in range(16)]
+        "Exp1": [path_convert(f'./log/Exp1-demo/{i}.monitor.csv')for i in range(8)],
+        "Exp2": [path_convert(f'./log/Exp2-demo/{i}.monitor.csv')for i in range(8)]
     }
     output_file = path_convert('./reward.png')
     plot_multi_reward_curves(log_labels, output_file, window_size=3)
